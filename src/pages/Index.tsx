c663d4630f05c03aedbc2af82b71ebea059a94d4
lalith128 <lalithrocky75@gmail.com>
2025-02-21 11:04:48 +0000

    Fix index page display
    The index page was rendering as blank. This commit addresses the issue.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,18 +4,22 @@ import { Hero } from "@/components/Hero";
 import { Features } from "@/components/Features";
 import { HowItWorks } from "@/components/HowItWorks";
 import { Footer } from "@/components/Footer";
+import { Toaster } from "sonner";
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Hero />
-        <Features />
-        <HowItWorks />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Toaster position="top-center" />
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Hero />
+          <Features />
+          <HowItWorks />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 };
 
